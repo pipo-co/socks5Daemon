9b6dbfc118f340d5175eddb1a4d3eb46ea6f6d66
Tobias <tbrandy@itba.edu.ar>
2020-11-03 00:52:26 -0300

    Activated Tests

--- test/main.c
+++ test/main.c
@@ -9,26 +9,27 @@ typedef Suite *(*suiteSupplier)(void);
 
 // Test files being exercised
 #include "tests/buffer_test.c"
-//#include "tests/socks5Test.c"
-//#include "tests/parser_test.c"
+#include "tests/parser_test.c"
 #include "tests/selector_test.c"
-//#include "tests/parser_utils_test.c"
+#include "tests/parser_utils_test.c"
 #include "tests/helloTest.c"
 #include "tests/helloParserTest.c"
-//#include "tests/requestParserTest.c"
+#include "tests/requestParserTest.c"
+// #include "tests/requestTest.c"
+//#include "tests/socks5Test.c"
 //#include "tests/stateMachineTest.c"
-//#include "tests/requestTest.c"
+
 
 // Tests being exercised
 static const suiteSupplier suiteSuppliers[] = {
-    // buffer_test_suite,
-    // parser_test_suite,
-    // parser_utils_test_suite,
+    buffer_test_suite,
+    parser_test_suite,
+    parser_utils_test_suite,
     hello_test_suite,
     hello_parser_test_suite,
-    // selector_test_suite,
-    //request_parser_test_suite,
-    //request_test_suite
+    selector_test_suite,
+    request_parser_test_suite,
+    // request_test_suite TODO: Broken
 };
 
 SRunner * test_srunner_init(void) {
