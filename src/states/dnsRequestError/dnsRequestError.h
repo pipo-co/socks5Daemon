b60895a677014600d3f8ffc82f67a3fa15e8689c
Ignacio <isagues@itba.edu.ar>
2020-11-06 23:24:42 -0300

    removed dnsRequestError state

--- src/states/dnsRequestError/dnsRequestError.h
+++
@@ -1,9 +0,0 @@
-#ifndef DNS_REQUEST_ERROR_H_a7f0b7011d5bc49decb646a7852c4531c07e17b5
-#define DNS_REQUEST_ERROR_H_a7f0b7011d5bc49decb646a7852c4531c07e17b5
-
-#include "socksDefs.h"
-#include "socks5/socks5SessionDefinition.h"
-
-SelectorStateDefinition dns_request_error_state_definition_supplier(void);
-
-#endif
