f0f91d443b429b02c21ba24a7cdaab3643a147e6
Ignacio <isagues@itba.edu.ar>
2020-11-09 12:43:54 -0300

    Added query and modification codes

--- src/client/clientCommandController.h
+++ src/client/clientCommandController.h
@@ -8,6 +8,35 @@ typedef struct CommandController {
 	void (*receiver)(int);	
 } CommandController;
 
+typedef enum ClientQuery {
+    CQ_LIST_USERS                              = 0x00,
+    CQ_TOTAL_HISTORIC_CONNECTIONS              = 0x01,
+    CQ_CURRENT_CONNECTIONS                     = 0x02,
+    CQ_MAX_CURRENT_CONECTIONS                  = 0x03,
+    CQ_TOTAL_BYTES_SENT                        = 0x04,
+    CQ_TOTAL_BYTES_RECEIVED                    = 0x05,
+    CQ_CONNECTED_USERS                         = 0x06,
+    CQ_USER_COUNT                              = 0x07,
+    CQ_BUFFER_SIZES                            = 0x08,
+    CQ_SELECTOR_TIMEOUT                        = 0x09,
+    CQ_CONNECTION_TIMEOUT                      = 0x0A,
+    CQ_USER_TOTAL_CONCURRENT_CONNECTIONS       = 0x0B,
+    CQ_INVALID_PARAM                           = 0xFE,
+    CQ_INVALID_QUERY                           = 0xFF,
+} ClientQuery;
+
+typedef enum ClientModification {
+    CM_ADD_USER                                = 0x00,
+    CM_REMOVE_USER                             = 0x01,
+    CM_TOGGLE_PASSWORD_SPOOFING                = 0x02,
+    CM_TOGGLE_CONNECTION_CLEAN_UN              = 0x03,
+    CM_SET_BUFFER_SIZE                         = 0x04,
+    CM_SET_SELECTOR_TIMEOUT                    = 0x05,
+    CM_SET_CONNECTION_TIMEOUT                  = 0x06,
+    CM_INVALID_MODIFICATION                    = 0xFF,
+} ClientModification;
+
+
 void client_command_controller_init(CommandController controllers[]);
 
 #endif
